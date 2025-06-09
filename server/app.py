04b1826e9fca15038f0c0ca1c9bff1959c39eb73
darlenenyambura <darlene.nyambura@student.moringaschool.com>
2025-06-10 01:53:45 +0300

    given up

--- server/app.py
+++ server/app.py
@@ -14,11 +14,26 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET'])
 def messages():
-    return ''
+    messages = Message.query.order_by(Message.created_at.asc()).all()
+    messages_list = [message.to_dict() for message in messages]
+    return make_response(jsonify(messages_list), 200)
+@app.route('/messages', methods=['POST'])
+def create_message():
+    data = request.get_json()
 
-@app.route('/messages/<int:id>')
+    try:
+        new_message = Message(
+            body=data['body'],
+            username=data['username']
+        )
+        db.session.add(new_message)
+        db.session.commit()
+        return make_response(new_message.to_dict(), 201)
+    except Exception as e:
+        return make_response({"error": str(e)}, 400)
+@app.route('/messages/<int:id>', methods=['PATCH'])
 def messages_by_id(id):
     return ''
 
--- server/models.py
+++ server/models.py
@@ -12,3 +12,6 @@ class Message(db.Model, SerializerMixin):
     __tablename__ = 'messages'
 
     id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable=False)
+    body = db.Column(db.String, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
